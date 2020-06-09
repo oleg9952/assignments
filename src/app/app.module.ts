73e75604a4a2f1ad79b5144ad6922ddf53e4bed3
oleg9952 <oleg.parhomenko2599@gmail.com>
2020-06-09 19:50:14 +0300

    styling - done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OddComponent } from './Components/odd/odd.component';
+import { EvenComponent } from './Components/even/even.component';
+import { ControlsComponent } from './Components/controls/controls.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OddComponent,
+    EvenComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule
