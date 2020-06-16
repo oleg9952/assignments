e83482f670d34292620b2aeb21dc290b1249fb00
oleg9952 <oleg.parhomenko2599@gmail.com>
2020-06-16 21:29:35 +0300

    completed

--- src/app/Components/product/product.component.ts
+++ src/app/Components/product/product.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ProductInterf } from 'src/app/app.component';
 
 @Component({
   selector: 'app-product',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product.component.scss']
 })
 export class ProductComponent implements OnInit {
+  @Input() product: ProductInterf;
+
+  @Output() onClone = new EventEmitter<ProductInterf>();
+  @Output() onDelete = new EventEmitter<string>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  handleClone(product: ProductInterf): void {
+    this.onClone.emit(product);
+  }
+  
+  handleDelete(productId: string): void {
+    this.onDelete.emit(productId);
+  }
 }
