defe4a8ac494053eaf7f9c75f655b1d0ca10739f
oleg9952 <oleg.parhomenko2599@gmail.com>
2020-06-16 20:38:01 +0300

    ui - done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormComponent } from './Components/form/form.component';
+import { ProductComponent } from './Components/product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
