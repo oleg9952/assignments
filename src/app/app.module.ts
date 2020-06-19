2d26a999ec97c0af971684e445e71a567b6b7de3
oleg9952 <oleg.parhomenko2599@gmail.com>
2020-06-19 19:53:47 +0300

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { FocusDirective } from './Directives/focus.directive';
+import { DStyleDirective } from './Directives/d-style.directive';
+import { HideDirective } from './Directives/hide.directive';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FocusDirective,
+    DStyleDirective,
+    HideDirective
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
